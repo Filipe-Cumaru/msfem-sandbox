b3ae980502b9e7227756041650f0363da1346739
Filipe-Cumaru <filipe.cumaru@ufpe.br>
2024-01-05 09:20:20 +0100

    feat(schwarz): add method to compute the partition of the domain into square subdomains

--- schwarz.py
+++ schwarz.py
@@ -18,6 +18,37 @@ class BaseTwoLevelASPreconditioner(object):
     def assemble(self):
         raise NotImplementedError()
 
+    def _compute_partitions(self):
+        """Sets `P`, a matrix that indicates the partition of the domain's
+        nodes into the non-overlapping subdomains.
+        """
+        # Since each subdomain is a square, the partition is computed by
+        # moving a "window" across the domain and assigning the nodes within
+        # the window to the subdomain.
+        ref_idx = np.arange(self.n + 1, dtype=int)
+        ref_window = np.concatenate([ref_idx + j * self.m for j in range(self.n + 1)])
+
+        row_idx = []
+        col_idx = []
+        P_values = []
+
+        for i in range(self.N**2):
+            # Horizontal and vertical displacement of the reference window.
+            displ_horiz, displ_vert = i % self.N, i // self.N
+
+            # The nodes in the subdomain \Omega_i.
+            Omega_i = (
+                ref_window + (displ_horiz * self.N) + (displ_vert * self.n * self.m)
+            )
+
+            row_idx.extend(Omega_i)
+            col_idx.extend(i * np.ones(len(Omega_i), dtype=int))
+            P_values.extend(np.ones(len(Omega_i)))
+
+        self.P = csc_matrix(
+            (P_values, (row_idx, col_idx)), shape=(self.m**2, self.N**2)
+        )
+
 
 class TwoLevelASPreconditioner(BaseTwoLevelASPreconditioner):
     def __init__(self, A, Phi, N, n, k) -> None:
