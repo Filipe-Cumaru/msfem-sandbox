bc17870d40a114712593ab9f8f30307d25eeae05
Filipe-Cumaru <filipe.cumaru@ufpe.br>
2024-03-07 10:53:18 +0100

    feat(msfem): add the null space type and dofs mapping to the attributes of the RGDSW coarse space

--- msfem.py
+++ msfem.py
@@ -1,12 +1,18 @@
 from scipy.sparse import lil_matrix, csc_matrix, eye, vstack, diags
 from scipy.integrate import quad
 from scipy.sparse.linalg import spsolve
+from enum import Enum
 import numpy as np
 
 
 def set_sparse_matrix_rows_to_value(M, rows, value):
     for row in rows:
         M.data[M.indptr[row] : M.indptr[row + 1]] = value
+
+
+class NullSpaceType(Enum):
+    DIFFUSION = 1
+    LINEAR_ELASTICITY = 2
 
 
 class MSBasisFunction(object):
@@ -42,10 +48,13 @@ class MSBasisFunction(object):
 
 
 class RGDSWCoarseSpace(MSBasisFunction):
-    def __init__(self, N, n, A, c):
+    def __init__(
+        self, N, n, A, c, null_space_type=NullSpaceType.DIFFUSION, dofs_map=None
+    ):
         super().__init__(N, n, c)
         self.A = A
         self.P, self.P_I, self.P_B = self._compute_partitions()
+
         self.coarse_nodes = np.array(
             [
                 i
@@ -58,11 +67,26 @@ class RGDSWCoarseSpace(MSBasisFunction):
         self.coarse_nodes_global_idx = (self.coarse_nodes % self.N) * (self.n - 1) + (
             self.coarse_nodes // self.N
         ) * (self.n - 1) * self.m
+
         self.xs, self.ys = np.meshgrid(
             np.linspace(0, 1, self.m), np.linspace(0, 1, self.m)
         )
         self.xs, self.ys = self.xs.flatten(), self.ys.flatten()
+
         self.D = self._assemble_inverse_distance_matrix()
+
+        if not isinstance(null_space_type, NullSpaceType):
+            raise ValueError(
+                "Invalid null space type. Must be taken from the enumeration NullSpaceType."
+            )
+        self.null_space_type = null_space_type
+
+        if dofs_map is not None:
+            self.dofs_map = dofs_map
+        elif self.null_space_type is NullSpaceType.DIFFUSION:
+            self.dofs_map = np.arange(self.m**2).reshape((self.m**2, 1))
+        elif self.null_space_type is NullSpaceType.LINEAR_ELASTICITY:
+            self.dofs_map = np.arange(3 * self.m**2).reshape((self.m**2, 3))
 
     def assemble_operator(self):
         interface_pou = self._compute_interface_pou()
