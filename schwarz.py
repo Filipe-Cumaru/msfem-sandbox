69e3d4957042a4a52fafb2f75bd7bb0cce07979f
Filipe-Cumaru <filipe.cumaru@ufpe.br>
2024-05-01 09:24:34 +0200

    refactor(schwarz): reformulate the preconditioner hierarchy
    The single-level AS is implemented in a separate class and the two-level variant is derived from this new class.

--- schwarz.py
+++ schwarz.py
@@ -4,20 +4,18 @@ from scipy.sparse.linalg import splu
 from msfem import NullSpaceType
 
 
-class BaseTwoLevelASPreconditioner(object):
-    """Base class used to implement the variants of the
-    two-level Additive Schwarz preconditioner.
+class SingleLevelASPreconditioner(object):
+    """An implementation of the single-level Additive Schwarz (AS)
+    preconditioner using MsFEM basis functions as a coarse space.
     """
 
     def __init__(
-        self, A, Phi, N, n, k, null_space_type=NullSpaceType.DIFFUSION, dofs_map=None
+        self, A, N, n, k, null_space_type=NullSpaceType.DIFFUSION, dofs_map=None
     ) -> None:
         """Constructor
 
         Args:
             A (sparse matrix): The matrix of the linear system.
-            Phi (sparse matrix): The MsFEM prolongation operator,
-                i.e., the assembled basis functions.
             N (int): Number of subdomains on each direction.
             n (int): Number of cells per subdomain on each direction.
             k (int): Number of layers for the overlap.
@@ -27,7 +25,6 @@ class BaseTwoLevelASPreconditioner(object):
             as a # nodes X # dofs numpy array. Optional, defaults to None.
         """
         self.A = A
-        self.Phi = Phi
         self.N = N
         self.n = n
         self.k = k
@@ -68,6 +65,31 @@ class BaseTwoLevelASPreconditioner(object):
 
         # A partition vector that splits the nodes into the overlapping subdmains.
         self.P_extended = self._compute_overlapping_partitions()
+
+        # The LU decompositions of each submatrix.
+        self.A_i_lu_decompostions = self._compute_local_lu_decompositions()
+
+    def apply(self, x):
+        """Applies the single-level additive Schwarz preconditioner to the current iterate
+        of a solver.
+
+        Args:
+            x (numpy.ndarray): The current iterate of the solver.
+
+        Returns:
+            numpy.ndarray: The preconditioned iterate.
+        """
+        y = np.zeros(self.num_dofs * (self.m**2))
+
+        for i in range(self.N**2):
+            Omega_i_extended = self.P_extended[:, i].nonzero()[0]
+            Omega_i_extended_dofs = self.dofs_map[Omega_i_extended, :].flatten()
+            A_i_lu = self.A_i_lu_decompostions[i]
+            x_i = x[Omega_i_extended_dofs]
+            y_i = A_i_lu.solve(x_i)
+            y[Omega_i_extended_dofs] += y_i
+
+        return y
 
     def _compute_partitions(self):
         """Sets `P`, a matrix that indicates the partition of the domain's
@@ -140,8 +162,18 @@ class BaseTwoLevelASPreconditioner(object):
             Omega_i_extended = np.union1d(Omega_i_prev, Omega_i_prev_neighbors)
         return Omega_i_extended
 
+    def _compute_local_lu_decompositions(self):
+        A_lu_decompositions = []
+        for i in range(self.N**2):
+            Omega_i_extended = self.P_extended[:, i].nonzero()[0]
+            Omega_i_extended_dofs = self.dofs_map[Omega_i_extended, :].flatten()
+            A_i = self.A[Omega_i_extended_dofs, Omega_i_extended_dofs[:, None]]
+            A_i_lu = splu(A_i)
+            A_lu_decompositions.append(A_i_lu)
+        return A_lu_decompositions
 
-class TwoLevelASPreconditioner(BaseTwoLevelASPreconditioner):
+
+class TwoLevelASPreconditioner(SingleLevelASPreconditioner):
     """An implementation of the two-level Additive Schwarz (AS)
     preconditioner using MsFEM basis functions as a coarse space.
     """
@@ -149,10 +181,8 @@ class TwoLevelASPreconditioner(BaseTwoLevelASPreconditioner):
     def __init__(
         self, A, Phi, N, n, k, null_space_type=NullSpaceType.DIFFUSION, dofs_map=None
     ) -> None:
-        super().__init__(
-            A, Phi, N, n, k, null_space_type=null_space_type, dofs_map=dofs_map
-        )
-        self.A_i_lu_decompostions = self._compute_local_lu_decompositions()
+        super().__init__(A, N, n, k, null_space_type=null_space_type, dofs_map=dofs_map)
+        self.Phi = Phi
         self.A_0_lu = splu(self.Phi @ (self.A @ self.Phi.transpose()))
 
     def apply(self, x):
@@ -165,16 +195,8 @@ class TwoLevelASPreconditioner(BaseTwoLevelASPreconditioner):
         Returns:
             numpy.ndarray: The preconditioned iterate.
         """
-        y = np.zeros(self.num_dofs * (self.m**2))
-
         # First level.
-        for i in range(self.N**2):
-            Omega_i_extended = self.P_extended[:, i].nonzero()[0]
-            Omega_i_extended_dofs = self.dofs_map[Omega_i_extended, :].flatten()
-            A_i_lu = self.A_i_lu_decompostions[i]
-            x_i = x[Omega_i_extended_dofs]
-            y_i = A_i_lu.solve(x_i)
-            y[Omega_i_extended_dofs] += y_i
+        y = super().apply(x)
 
         # Second level.
         x_0 = self.Phi @ x
@@ -182,13 +204,3 @@ class TwoLevelASPreconditioner(BaseTwoLevelASPreconditioner):
         y += self.Phi.transpose() @ y_0
 
         return y
-
-    def _compute_local_lu_decompositions(self):
-        A_lu_decompositions = []
-        for i in range(self.N**2):
-            Omega_i_extended = self.P_extended[:, i].nonzero()[0]
-            Omega_i_extended_dofs = self.dofs_map[Omega_i_extended, :].flatten()
-            A_i = self.A[Omega_i_extended_dofs, Omega_i_extended_dofs[:, None]]
-            A_i_lu = splu(A_i)
-            A_lu_decompositions.append(A_i_lu)
-        return A_lu_decompositions
